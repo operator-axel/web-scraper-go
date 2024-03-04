20519730404fe338c5ece2c481cfbcac527499f2
operator-axel <abolduc0917@gmail.com>
2024-03-04 10:10:10 -0500

    updated to just naming file and providing URL - automatically will only collect links from first substring

--- scraper.go
+++ scraper.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"net/url"
 	"os"
 	"strings"
 
@@ -11,63 +12,52 @@ import (
 )
 
 func main() {
+	fmt.Print("URL to scrape: ")
+	var urlToScrape string
+	fmt.Scan(&urlToScrape)
+
+	parsedURL, err := url.Parse(urlToScrape)
+	if err != nil {
+		log.Fatalf("Failed to parse URL: %s\n", err)
+	}
+
+	// Automatically determine the link filter from the URL path
+	// Extracting the first path segment as the link filter
+	pathSegments := strings.Split(parsedURL.Path, "/")
+	var linkFilter string
+	if len(pathSegments) > 1 {
+		linkFilter = "/" + pathSegments[1] // Adjust this logic based on your needs
+	}
+
 	reader := bufio.NewReader(os.Stdin)
-
-	var fileName string
 	fmt.Print("What should the name of the file be? ")
-	fileName, _ = reader.ReadString('\n')
+	fileName, _ := reader.ReadString('\n')
 	fileName = strings.TrimSpace(fileName)
-
 	filePath := fmt.Sprintf("/home/ch40s/Obsidian/dolos/%s.md", fileName)
 	file, err := os.Create(filePath)
-
 	if err != nil {
 		log.Fatalf("Cannot create file %q: %s\n", filePath, err)
 		return
 	}
 	defer file.Close()
 
-	var domain string
-	fmt.Print("Provide the allowed domains you want to scrape: (e.g., archwiki.com) ")
-	domain, _ = reader.ReadString('\n')
-	domain = strings.TrimSpace(domain)
-
 	c := colly.NewCollector(
-		colly.AllowedDomains(domain),
+		colly.AllowedDomains(parsedURL.Hostname()),
 	)
-
-	fmt.Print("Enter a substring that must be present in the links (leave blank for no filter): ")
-	linkFilter, _ := reader.ReadString('\n')
-	linkFilter = strings.TrimSpace(linkFilter)
 
 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
 		link := e.Attr("href")
 
-		// Process links that contain the specified filter substring
-		if linkFilter == "" || strings.Contains(link, linkFilter) {
+		if strings.Contains(link, linkFilter) {
 			absoluteURL := e.Request.AbsoluteURL(link)
-
-			parts := strings.Split(link, "/")
-			var linkText string
-			for i := len(parts) - 1; i >= 0; i-- {
-				if parts[i] != "" {
-					linkText = parts[i]
-					break
-				}
-			}
-			mdLink := "- [" + linkText + "](" + absoluteURL + ")\n"
-
+			mdLink := fmt.Sprintf("- [%s](%s)\n", link, absoluteURL)
 			if _, err := file.WriteString(mdLink); err != nil {
-				log.Printf("Failed to write lesson URL to file: %s\n", err)
+				log.Printf("Failed to write link to file: %s\n", err)
 			}
 		}
 	})
 
-	var urlToScrape string
-	fmt.Print("URL to scrape: ")
-	fmt.Scan(&urlToScrape)
 	err = c.Visit(urlToScrape)
-
 	if err != nil {
 		log.Fatalf("Failed to visit site: %s\n", err)
 		return
